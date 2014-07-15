7d509ce896314dcb93b9040a9555dc9d7b29386c
Frank-Robert Kline <frank-robert.kline@hightail.com>
2014-07-15 16:11:49 -0700

    tomcat starts with JMX, JDWP, manager-apps configured

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -4,11 +4,13 @@ node.default['tomcat']['user'] = 'tomcat'
 node.default['tomcat']['group'] = 'tomcat'
 node.default['tomcat']['version'] = '7.0.54'
 node.default['tomcat']['install_directory'] = '/opt'
-node.default['tomcat']['default_java_opts'] = [
-  '-server',
-  '-Djava.awt.headless=true'
+node.default['tomcat']['java_opts'] = [
+  '-Djava.awt.headless=true',
+  '-Dfile.encoding=UTF-8',
+  '-server'
 ]
-node.default['tomcat']['additional_java_opts'] = []
+node.default['tomcat']['catalina_opts'] = [
+]
 
 # Manager
 node.default['tomcat']['webapps']['manager']['deploy'] = true
@@ -17,8 +19,33 @@ node.default['tomcat']['webapps']['manager']['admin-script']['pw'] = 'script'
 node.default['tomcat']['webapps']['manager']['admin-gui']['enabled'] = true
 node.default['tomcat']['webapps']['manager']['admin-gui']['pw'] = 'gui'
 
-## server.xml configuration
-# default['tomcat-all']['port'] = '8080'
-# default['tomcat-all']['shutdown_port'] = '8005'
-# default['tomcat-all']['max_threads'] = '100'
-# default['tomcat-all']['min_spare_threads'] = '10'
+# JMX
+default['tomcat']['jmx']['rmi_registry_port'] = '10001'
+default['tomcat']['jmx']['rmi_server_port'] = '10002'
+default['tomcat']['jmx']['rmi_bind_address'] = 'localhost'
+default['tomcat']['jmx']['use_local_ports'] = false
+default['tomcat']['jmx']['ssl'] = false
+default['tomcat']['jmx']['authenticate'] = true
+default['tomcat']['jmx']['password_file'] =
+  '$CATALINA_HOME/conf/jmxremote.password'
+default['tomcat']['jmx']['access_file'] =
+  '$CATALINA_HOME/conf/jmxremote.access'
+default['tomcat']['jmx']['username'] = 'admin-jmx'
+default['tomcat']['jmx']['password'] = 'jmx'
+
+# JDWP
+default['tomcat']['jdwp']['enabled'] = true
+default['tomcat']['jdwp']['suspend'] = false
+default['tomcat']['jdwp']['port'] = '10003'
+
+# server.xml
+default['tomcat']['shutdown_port'] = '8005'
+default['tomcat']['shutdown'] = 'shutdown'
+default['tomcat']['executor']['max_threads'] = '100'
+default['tomcat']['executor']['min_spare_threads'] = '10'
+default['tomcat']['connector']['http']['protocol'] =
+  'org.apache.coyote.http11.Http11NioProtocol'
+default['tomcat']['connector']['http']['port'] = '8080'
+default['tomcat']['connector']['http']['redirect_port'] = '8443'
+default['tomcat']['connector']['http']['connection_timeout'] = '20000'
+default['tomcat']['connector']['http']['uri_encoding'] = 'UTF-8'
--- cookbook/recipes/tomcat.rb
+++ cookbook/recipes/tomcat.rb
@@ -8,9 +8,12 @@ group node['tomcat']['group']
 
 # Create tomcat user
 user node['tomcat']['user'] do
+  supports :manage_home => true
   group node['tomcat']['group']
   system true
+  home '/home/tomcat'
   shell '/bin/bash'
+  comment 'Tomcat user'
 end
 
 # Download and unpack tomcat
@@ -18,8 +21,12 @@ download_url =
   'http://archive.apache.org/dist/tomcat/tomcat-7/'\
   "v#{node['tomcat']['version']}/bin/apache-tomcat-"\
   "#{node['tomcat']['version']}.tar.gz"
+extras_download_url =
+  'http://archive.apache.org/dist/tomcat/tomcat-7/'\
+  "v#{node['tomcat']['version']}/bin/extras"
 ark 'tomcat' do
   url download_url
+  checksum 'f0316c128881c4df384771dc0da8f8e80d861385798e57d22fd4068f48ab8724'
   version node['tomcat']['version']
   prefix_root node['tomcat']['install_directory']
   prefix_home node['tomcat']['install_directory']
@@ -49,35 +56,102 @@ end
 # Add the version file
 template "#{node['tomcat']['install_directory']}/tomcat/VERSION" do
   source 'VERSION.erb'
-  mode '755'
+  mode '644'
   notifies :restart, 'service[tomcat]', :delayed
+end
+
+# Download an add JmxRemoteLifecycleListener
+# http://tomcat.apache.org/tomcat-7.0-doc/config/listeners.html
+jmx_remote_jar =
+  "#{node['tomcat']['install_directory']}/tomcat/"\
+  'lib/catalina-jmx-remote.jar'
+remote_file jmx_remote_jar do
+  source "#{extras_download_url}/catalina-jmx-remote.jar"
+  mode '644'
+  notifies :restart, 'service[tomcat]', :delayed
+end
+
+# Update CATALINA_OPTS
+node.normal['tomcat']['catalina_opts'] =
+  node['tomcat']['catalina_opts'].concat([
+    '-Dcom.sun.management.jmxremote.ssl'\
+      "=#{node['tomcat']['jmx']['ssl']}",
+    '-Dcom.sun.management.jmxremote.authenticate'\
+      "=#{node['tomcat']['jmx']['authenticate']}",
+    '-Dcom.sun.management.jmxremote.password.file'\
+      "=#{node['tomcat']['jmx']['password_file']}",
+    '-Dcom.sun.management.jmxremote.access.file'\
+      "=#{node['tomcat']['jmx']['access_file']}"
+  ])
+
+# Enable JDWP
+# http://docs.oracle.com/javase/1.5.0/docs/guide/jpda/jdwp-spec.html
+if node['tomcat']['jdwp']['enabled']
+  if node['tomcat']['jdwp']['suspend']
+    node.normal['tomcat']['catalina_opts'] =
+      node['tomcat']['catalina_opts'].concat([
+        '-Xdebug',
+        '-Xrunjdwp:transport=dt_socket,address='\
+          "#{node['tomcat']['jdwp']['port']},suspend=y,server=y"
+      ])
+  else
+    node.normal['tomcat']['catalina_opts'] =
+      node['tomcat']['catalina_opts'].concat([
+        '-Xdebug',
+        '-Xrunjdwp:transport=dt_socket,address='\
+          "#{node['tomcat']['jdwp']['port']},suspend=n,server=y"
+      ])
+  end
 end
 
 # Enable the tomcat service
 template '/etc/init.d/tomcat' do
-  source 'init.conf.erb'
+  source 'tomcat.erb'
   mode '755'
   notifies :restart, 'service[tomcat]', :delayed
 end
 
 # Tomcat catalina configuration
 template "#{node['tomcat']['install_directory']}/tomcat/bin/catalina.sh" do
-  source 'catalina.conf.erb'
+  source 'catalina.sh.erb'
   mode '755'
   notifies :restart, 'service[tomcat]', :delayed
 end
 
 # Tomcat users configuration
-tomcat_users_file = 
+tomcat_users_file =
   "#{node['tomcat']['install_directory']}/tomcat/"\
   'conf/tomcat-users.xml'
 template tomcat_users_file do
   source 'tomcat-users.xml.erb'
-  mode '755'
+  mode '644'
   notifies :restart, 'service[tomcat]', :delayed
 end
 
-# Start the tomcat service
+# server.xml
+template "#{node['tomcat']['install_directory']}/tomcat/conf/server.xml" do
+  source 'server.xml.erb'
+  mode '644'
+  notifies :restart, 'service[tomcat]', :delayed
+end
+
+# JMX
+password_file =
+  "#{node['tomcat']['install_directory']}/tomcat/conf/jmxremote.password"
+access_file =
+  "#{node['tomcat']['install_directory']}/tomcat/conf/jmxremote.access"
+template password_file do
+  source 'jmxremote.password.erb'
+  mode '644'
+  notifies :restart, 'service[tomcat]', :delayed
+end
+template access_file do
+  source 'jmxremote.access.erb'
+  mode '644'
+  notifies :restart, 'service[tomcat]', :delayed
+end
+
+# Enable the tomcat service
 service 'tomcat' do
   supports :restart => true, :start => true, :stop => true
   action [:enable]
