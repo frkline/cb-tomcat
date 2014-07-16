29b1325035d0d0d015fc4762b061d5b192c32ac6
Frank-Robert Kline <frank-robert.kline@hightail.com>
2014-07-16 00:52:33 -0700

    added APR

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -7,10 +7,18 @@ node.default['tomcat']['install_directory'] = '/opt'
 node.default['tomcat']['java_opts'] = [
   '-Djava.awt.headless=true',
   '-Dfile.encoding=UTF-8',
-  '-server'
+  '-server',
+  '-XX:-HeapDumpOnOutOfMemoryError',
+  '-XX:-OmitStackTraceInFastThrow',
+  '-Djava.net.preferIPv4Stack=true'
 ]
 node.default['tomcat']['catalina_opts'] = [
 ]
+
+# APR
+node.default['tomcat']['apr']['version'] = '1.5.1'
+node.default['tomcat']['apr-util']['version'] = '1.5.3'
+node.default['tomcat']['apr']['install_directory'] = '/usr/local'
 
 # Manager
 node.default['tomcat']['webapps']['manager']['deploy'] = true
@@ -20,32 +28,88 @@ node.default['tomcat']['webapps']['manager']['admin-gui']['enabled'] = true
 node.default['tomcat']['webapps']['manager']['admin-gui']['pw'] = 'gui'
 
 # JMX
-default['tomcat']['jmx']['rmi_registry_port'] = '10001'
-default['tomcat']['jmx']['rmi_server_port'] = '10002'
-default['tomcat']['jmx']['rmi_bind_address'] = 'localhost'
-default['tomcat']['jmx']['use_local_ports'] = false
-default['tomcat']['jmx']['ssl'] = false
-default['tomcat']['jmx']['authenticate'] = true
-default['tomcat']['jmx']['password_file'] =
+node.default['tomcat']['jmx']['rmi_registry_port'] = '10001'
+node.default['tomcat']['jmx']['rmi_server_port'] = '10002'
+node.default['tomcat']['jmx']['rmi_bind_address'] = 'localhost'
+node.default['tomcat']['jmx']['use_local_ports'] = false
+node.default['tomcat']['jmx']['ssl'] = false
+node.default['tomcat']['jmx']['authenticate'] = true
+node.default['tomcat']['jmx']['password_file'] =
   '$CATALINA_HOME/conf/jmxremote.password'
-default['tomcat']['jmx']['access_file'] =
+node.default['tomcat']['jmx']['access_file'] =
   '$CATALINA_HOME/conf/jmxremote.access'
-default['tomcat']['jmx']['username'] = 'admin-jmx'
-default['tomcat']['jmx']['password'] = 'jmx'
+node.default['tomcat']['jmx']['username'] = 'admin-jmx'
+node.default['tomcat']['jmx']['password'] = 'jmx'
 
 # JDWP
-default['tomcat']['jdwp']['enabled'] = true
-default['tomcat']['jdwp']['suspend'] = false
-default['tomcat']['jdwp']['port'] = '10003'
+node.default['tomcat']['jdwp']['enabled'] = true
+node.default['tomcat']['jdwp']['suspend'] = false
+node.default['tomcat']['jdwp']['port'] = '10003'
 
 # server.xml
-default['tomcat']['shutdown_port'] = '8005'
-default['tomcat']['shutdown'] = 'shutdown'
-default['tomcat']['executor']['max_threads'] = '100'
-default['tomcat']['executor']['min_spare_threads'] = '10'
-default['tomcat']['connector']['http']['protocol'] =
-  'org.apache.coyote.http11.Http11NioProtocol'
-default['tomcat']['connector']['http']['port'] = '8080'
-default['tomcat']['connector']['http']['redirect_port'] = '8443'
-default['tomcat']['connector']['http']['connection_timeout'] = '20000'
-default['tomcat']['connector']['http']['uri_encoding'] = 'UTF-8'
+# Connector options from: http://tomcat.apache.org/tomcat-7.0-doc/config/http.html
+node.default['tomcat']['shutdown_port'] = '8005'
+node.default['tomcat']['shutdown'] = 'shutdown'
+node.default['tomcat']['executor']['max_threads'] = '100'
+node.default['tomcat']['executor']['min_spare_threads'] = '10'
+node.default['tomcat']['connector']['apr']['enabled'] = true
+node.default['tomcat']['connector']['ssl_engine'] = 'off'
+node.default['tomcat']['connector']['http']['allow_trace'] = 'false'
+node.default['tomcat']['connector']['http']['async_timeout'] = '10000'
+node.default['tomcat']['connector']['http']['enable_lookups'] = 'false'
+node.default['tomcat']['connector']['http']['max_header_count'] = '100'
+node.default['tomcat']['connector']['http']['max_parameter_count'] = '10000'
+node.default['tomcat']['connector']['http']['max_post_size'] = '2097152'
+node.default['tomcat']['connector']['http']['max_save_post_size'] = '4096'
+node.default['tomcat']['connector']['http']['parse_body_methods'] = 'POST'
+node.default['tomcat']['connector']['http']['port'] = '8080'
+node.default['tomcat']['connector']['http']['protocol'] =
+  'org.apache.coyote.http11.Http11AprProtocol'
+node.default['tomcat']['connector']['http']['proxy_name'] = ''
+node.default['tomcat']['connector']['http']['proxy_port'] =
+  node['tomcat']['connector']['http']['port']
+node.default['tomcat']['connector']['http']['redirect_port'] = '8443'
+node.default['tomcat']['connector']['http']['scheme'] = 'http'
+node.default['tomcat']['connector']['http']['secure'] = 'false'
+node.default['tomcat']['connector']['http']['uri_encoding'] = 'UTF-8'
+node.default['tomcat']['connector']['http']['body_encoding_for_uri'] = 'false'
+node.default['tomcat']['connector']['http']['use_ipv_hosts'] = 'false'
+node.default['tomcat']['connector']['http']['xpoweredby'] = 'false'
+node.default['tomcat']['connector']['http']['accept_count'] = '100'
+node.default['tomcat']['connector']['http']['acceptor_thread_count'] = '2'
+node.default['tomcat']['connector']['http']['acceptor_thread_priority'] = '5'
+node.default['tomcat']['connector']['http']['address'] = ''
+node.default['tomcat']['connector']['http']['bind_on_init'] = 'true'
+node.default['tomcat']['connector']['http']['compressable_mime_type'] = ''
+node.default['tomcat']['connector']['http']['compression'] = 'off'
+node.default['tomcat']['connector']['http']['compression_min_size'] = '2048'
+node.default['tomcat']['connector']['http']['connection_linger'] = '-1'
+node.default['tomcat']['connector']['http']['connection_timeout'] = '20000'
+node.default['tomcat']['connector']['http']['connection_upload_timeout'] =
+  '300000'
+node.default['tomcat']['connector']['http']['disable_upload_timeout'] = 'true'
+node.default['tomcat']['connector']['http']['executor'] = 'tomcatThreadPool'
+node.default['tomcat']['connector']['http']['executor_kill_timeout'] = ''
+node.default['tomcat']['connector']['http']['keep_alive_timeout'] = '5000'
+node.default['tomcat']['connector']['http']['max_connections'] = ''
+node.default['tomcat']['connector']['http']['max_extension_size'] = '8192'
+node.default['tomcat']['connector']['http']['max_http_header_size'] = '8192'
+node.default['tomcat']['connector']['http']['max_keep_alive_requests'] = '1'
+node.default['tomcat']['connector']['http']['max_threads'] = '200'
+node.default['tomcat']['connector']['http']['max_trailer_size'] = '8192'
+node.default['tomcat']['connector']['http']['min_spare_threads'] = ''
+node.default['tomcat']['connector']['http']['no_compression_user_agents'] = ''
+node.default['tomcat']['connector']['http']['processor_cache'] = '200'
+node.default['tomcat']['connector']['http']['restricted_user_agents'] = ''
+node.default['tomcat']['connector']['http']['server'] = 'server'
+node.default['tomcat']['connector']['http']['socket_buffer'] = '9000'
+node.default['tomcat']['connector']['http']['ssl_enabled'] = 'false'
+node.default['tomcat']['connector']['http']['tcp_no_delay'] = 'true'
+node.default['tomcat']['connector']['http']['thread_priority'] = '5'
+node.default['tomcat']['connector']['http']['defer_accept'] = 'true'
+node.default['tomcat']['connector']['http']['poller_size'] = '8192'
+node.default['tomcat']['connector']['http']['poll_time'] = '2000'
+node.default['tomcat']['connector']['http']['sendfile_size'] = '1024'
+node.default['tomcat']['connector']['http']['thread_priority'] = '5'
+node.default['tomcat']['connector']['http']['use_comet'] = 'false'
+node.default['tomcat']['connector']['http']['use_sendfile'] = 'false'
--- cookbook/recipes/tomcat.rb
+++ cookbook/recipes/tomcat.rb
@@ -156,3 +156,71 @@ service 'tomcat' do
   supports :restart => true, :start => true, :stop => true
   action [:enable]
 end
+
+# Install APR
+bash 'reload profile prior to apr installation' do
+  code 'source /etc/profile'
+end
+apr_download_url =
+  'http://archive.apache.org/dist/apr/'\
+  "apr-#{node['tomcat']['apr']['version']}.tar.gz"
+apr_util_download_url =
+  'http://archive.apache.org/dist/apr/'\
+  "apr-util-#{node['tomcat']['apr-util']['version']}.tar.gz"
+ark 'apr' do
+  url apr_download_url
+  checksum '94b1c9d9835cc9e902838b95d62ecc9a39b698f23e3e706812ec65a78ba41af7'
+  version node['tomcat']['apr']['version']
+  prefix_root node['tomcat']['apr']['install_directory']
+  prefix_home node['tomcat']['apr']['install_directory']
+  owner 'root'
+  action :configure
+end
+bash 'make apr' do
+  user 'root'
+  cwd node['tomcat']['apr']['install_directory']
+  code <<-EOH
+  cd apr-#{node['tomcat']['apr']['version']}
+  make && make install
+  EOH
+end
+ark 'apr-util' do
+  url apr_util_download_url
+  checksum '76db34cb508e346e3bf69347c29ed1500bf0b71bcc48d54271ad9d1c25703743'
+  version node['tomcat']['apr-util']['version']
+  prefix_root node['tomcat']['apr']['install_directory']
+  prefix_home node['tomcat']['apr']['install_directory']
+  autoconf_opts ['--with-apr=/usr/local/apr/']
+  owner 'root'
+  action :configure
+end
+ark 'tomcat-native' do
+  url "file://#{node['tomcat']['install_directory']}/"\
+      'tomcat/bin/tomcat-native.tar.gz'
+  version node['tomcat']['version']
+  prefix_root node['tomcat']['apr']['install_directory']
+  prefix_home node['tomcat']['apr']['install_directory']
+  strip_components 3
+  autoconf_opts ['--with-apr=/usr/local/apr/']
+  owner 'root'
+  action :configure
+end
+bash 'make apr utils and tomcat native' do
+  user 'root'
+  cwd node['tomcat']['apr']['install_directory']
+  code <<-EOH
+  cd apr-util-#{node['tomcat']['apr-util']['version']}
+  make && make install
+  cd #{node['tomcat']['apr']['install_directory']}
+  cd tomcat-native-#{node['tomcat']['version']}
+  make && make install
+  EOH
+end
+template '/etc/profile.d/apr.sh' do
+  source 'apr.sh.erb'
+  mode '755'
+  notifies :restart, 'service[tomcat]', :delayed
+end
+bash 'reload profile' do
+  code 'source /etc/profile'
+end
